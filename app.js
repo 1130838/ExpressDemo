84330bc9029ff0cf15abee1e7598d1e07b33e3bd
bruno devesa <1130838@isep.ipp.pt>
2015-08-13 23:10:30 +0100

    lot of stuff in here..

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var about = require('./routes/about');
 var users = require('./routes/users');
 
 var app = express();
+
+app.locals.points = "8,713";
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
